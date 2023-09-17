9cd33eabf2b32c4e8700aef7024d5d8405e36b5c
Thatboringguy <106856762+Thatboringguy@users.noreply.github.com>
2023-09-17 12:51:46 +0530

    Add files via upload

---
+++ #simple calculator.py
@@ -0,0 +1,38 @@
+#simple calculator 
+def add(num1,num2) :
+    return num1 + num2 
+def subtract (num1,num2):
+    return num1 - num2 
+def multiply (num1,num2):
+    return num1 * num2 
+def divide (num1,num2):
+    return num1 / num2 
+
+print("please select operation \n"
+       " 1.add\n"
+       " 2.subtract\n"
+       " 3.multiply\n"
+       " 4.divde\n")
+
+select = int(input("select operations form 1, 2, 3, 4, : "))
+number_1 = int(input("enter the 1st number: "))
+number_2 = int(input("enter the 2nd number: "))
+
+if select == 1 :
+    print( number_1, "+", number_2, "=",
+                       add(number_1, number_2))
+    
+elif select == 2 :
+    print( number_1, "-", number_2, "=",
+                    subtract(number_1, number_2) )
+
+elif select == 3 : 
+    print( number_1, "*", number_2, "=",
+                    multiply(number_1, number_2))
+    
+elif select == 4 :
+    print( number_1, "/", number_2, "=",
+                    divide(number_1, number_2))
+    
+else:
+    print("invalid input")     
